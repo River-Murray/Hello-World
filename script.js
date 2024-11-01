b36e38b530854fc69eaf6b9b9115bf3c79705b98
River <claire.murray@stu.ocsb.ca>
2024-11-01 14:12:22 +0000

    hjtyhm

---
+++ script.js
@@ -0,0 +1,18 @@
+function main(){
+    let x = prompt("say  hi")
+    if (x==hi()){
+        codeHSMadeMeMakeThisAndImNotGonnaMakeItFunctionalBecauseCodeHSIsStupidInItsRequirements();
+    }
+
+}
+
+function codeHSMadeMeMakeThisAndImNotGonnaMakeItFunctionalBecauseCodeHSIsStupidInItsRequirements() {
+    console.log("(╯°□°)╯︵ ┻━┻");
+    cannot();
+    // I am so done
+}
+
+function cannot() {
+    codeHSMadeMeMakeThisAndImNotGonnaMakeItFunctionalBecauseCodeHSWontIsStupidInItsRequirements();
+}
+main();
